28d20cb5ee08d7b723de7e38eab8af2e9bbe6080
zach fine <zfine@utexas.edu>
2017-05-22 16:16:14 -0500

    Switch to new website

--- README.md
+++
@@ -1,5 +0,0 @@
-# DropText
-Use 📺 to 🔥 up your text 
-
-For usage information see:
-(http://siggisim.github.io/DropText/)
--- index.php
+++ index.php
@@ -1,42 +1,84 @@
-<?
-$config = array(
-  'text'    => isset($_GET['text'])   ? $_GET['text']   : 'DropText',
-  'video'   => isset($_GET['video'])  ? $_GET['video']  : 'A7MiR053DE8',
-  'size'    => isset($_GET['size'])   ? $_GET['size']   : '200',
-  'start'   => isset($_GET['start'])  ? $_GET['start']  : '55',
-  'font'    => isset($_GET['font'])   ? $_GET['font']   : 'Roboto',
-  'weight'  => isset($_GET['weight']) ? $_GET['weight'] : '900'
-);
-?>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<html>
-  <head>
-    <title>DropText</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="dropText.css" rel="stylesheet" type="text/css">
-    <script src="dropText.js" type="text/javascript"></script>
-    <style>
-      #drop-text {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="drop-text"></div>
-    <script>
-      dropText({
-        id:     'drop-text',
-        text:   '<?=$config['text']?>',
-        video:  '<?=$config['video']?>',
-        size:   '<?=$config['size']?>',
-        start:  '<?=$config['start']?>',
-        font:   '<?=$config['font']?>',
-        weight: '<?=$config['weight']?>'
-      });
-    </script>
-  </body>
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="https://use.fontawesome.com/d55da1856e.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+        <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Londrina+Sketch|Source+Sans+Pro" rel="stylesheet">
+
+
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container-fluid" style="padding:0;">
+            <div class="row">
+                <div class="col-xl-5 col-lg-12" style="padding: 0">
+                    <img src="img/profile2.jpg" class="img-fluid rounded d-block" alt="Responsive image">
+                </div>
+                <div class="col-xl-7 col-lg-12 text-center" style="padding: 0;padding-top:250px;">
+                    <h1 class="display-4 dancing"> hi, my name is...</h1>
+                    <h1 class="display-3 name-text"> Lisa Vo </h1>
+
+                    <h5 class="mb-2">
+                        Tetris Beast | Avid Foodie | Urban Explorer | NetFlix Addict
+                    </h5>
+
+
+
+                    <p style="width:50%;margin:0 auto;">
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    </p>
+
+
+                    <div class="mt-4">
+                        <a class="lisa-blue" href="https://www.linkedin.com/in/lisakimvo/"><i class="fa fa-2x fa-linkedin-square" aria-hidden="true"></i></a>
+                        <a class="lisa-blue" href="https://medium.com/@whatserfacelisa"><i class="fa fa-2x fa-medium" aria-hidden="true"></i></a>
+                        <a class="lisa-blue" href="mailto:vo.lisa.kim@icloud.com?Subject=Hello%20again"><i class="fa fa-2x fa-envelope-square" aria-hidden="true"></i></a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+        <script src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-{{JQUERY_VERSION}}.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+
+
+
+
+    </body>
 </html>
